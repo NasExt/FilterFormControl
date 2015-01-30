5f512f8915e3326a4740a9ae875be1165fa3c962
Jáchym Toušek <enumag@gmail.com>
2015-01-30 14:12:50 +0100

    Removed redirect

--- src/NasExt/Controls/FilterFormControl.php
+++ src/NasExt/Controls/FilterFormControl.php
@@ -209,10 +209,6 @@ class FilterFormControl extends Control
 		$values = $button->getForm()->getValues(TRUE);
 		$this->saveData($values);
 		$this->onFilter($this, $this->getData());
-
-		if (!$this->presenter->isAjax()) {
-			$this->presenter->redirect('this');
-		}
 	}
 
 
@@ -226,10 +222,6 @@ class FilterFormControl extends Control
 		$form->setValues(array(), TRUE);
 		$this->data = NULL;
 		$this->onReset($this, $this->getData());
-
-		if (!$this->presenter->isAjax()) {
-			$this->presenter->redirect('this');
-		}
 	}
 
 
